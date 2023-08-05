0096e04627d60d987d01b2f5be9e624192052c96
unknown <khaledguesmi889@gmail.com>
2023-08-05 21:57:50 +0100

    video-preview

---
+++ Week2/Day 5/Practice/video-preview/Script.js
@@ -0,0 +1,8 @@
+console.log("page loaded...");
+const video = document.getElementById('pexels-cottonbro-6853337 (2160p).mp4');
+function playVideo() {
+  video.play();
+}
+function pauseVideo() {
+  video.pause();
+}
